890e6682b34854136e47ad71dc1b8531423516e7
eiixy <990656271@qq.com>
2024-06-14 16:22:09 +0800

    add kafka pkg

---
+++ pkg/kafka/sync_producer.go
@@ -0,0 +1,25 @@
+package kafka
+
+import (
+	"github.com/IBM/sarama"
+	"log"
+)
+
+func NewSyncProducer(addrs []string, opts ...ConfigOption) (sarama.SyncProducer, error) {
+	cfg := sarama.NewConfig()
+	cfg.Version = sarama.V2_1_1_0
+	cfg.Producer.RequiredAcks = sarama.WaitForAll
+	cfg.Producer.Return.Successes = true
+	cfg.Producer.Partitioner = sarama.NewHashPartitioner
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
+	client, err := sarama.NewSyncProducer(addrs, cfg)
+	if err != nil {
+		log.Println("producer closed, err:", err)
+		return nil, err
+	}
+
+	return client, nil
+}
---
+++ pkg/kafka/sync_producer_test.go
@@ -0,0 +1,28 @@
+package kafka
+
+import (
+	"fmt"
+	"github.com/IBM/sarama"
+	"log"
+	"strconv"
+	"testing"
+)
+
+func TestNewSyncProducer(t *testing.T) {
+	sp, err := NewSyncProducer([]string{"127.0.0.1:9093"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < 30; i++ {
+		partition, offset, err := sp.SendMessage(&sarama.ProducerMessage{
+			Topic: "test",
+			Key:   sarama.StringEncoder(strconv.Itoa(i)),
+			Value: sarama.StringEncoder(fmt.Sprintf("test data %d", i)),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("sync_producer: partition=%d offset=%d\n", partition, offset)
+	}
+}
